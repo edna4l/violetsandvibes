b5f7ae2ca7e690a4d54068b33d5bf9ae129acbe1
emchavarria1 <111106340+emchavarria1@users.noreply.github.com>
2026-02-07 23:53:01 -0800

    Replace react-swc plugin with react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-// vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1000, // Set the limit in KiB (e.g., 1000 KiB = 1 MB)
-  },
-});
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
